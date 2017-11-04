8145ab9ae721ec99d78359e81d390a6616158d26
Johan Larsson <johan_gavle1@hotmail.com>
2017-11-04 22:01:05 +0100

    Intial.

---
+++ TypeConverterBox/CustomTimeSpanConverter.cs
@@ -0,0 +1,24 @@
+﻿namespace TypeConverterBox
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    public class CustomTimeSpanConverter : TimeSpanConverter
+    {
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string text)
+            {
+                var match = Regex.Match(text, @"(?<ms>\d+) ms");
+                if (match.Success)
+                {
+                    return TimeSpan.FromMilliseconds(int.Parse(match.Groups["ms"].Value));
+                }
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
---
+++ TypeConverterBox/FooControl.cs
@@ -0,0 +1,22 @@
+﻿namespace TypeConverterBox
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+    public class FooControl : Control
+    {
+        public static readonly DependencyProperty DurationProperty = DependencyProperty.Register(
+            nameof(Duration),
+            typeof(TimeSpan),
+            typeof(FooControl),
+            new PropertyMetadata(default(TimeSpan)));
+
+        [TypeConverter(typeof(CustomTimeSpanConverter))]
+        public TimeSpan Duration
+        {
+            get => (TimeSpan)GetValue(DurationProperty);
+            set => SetValue(DurationProperty, value);
+        }
+    }
+}
